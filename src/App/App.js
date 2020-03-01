96e4ee5213ee8b5413b6c65b95cfd496bb44f455
Greg Stephen <GregAStephen@gmail.com>
2020-03-01 14:33:02 -0600

    got it set up and added the first problem solving example

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import ProblemSolvingPatterns from './ProblemSolvingPatterns';
+
+function App() {
+ console.error(ProblemSolvingPatterns.same([1,2,3,2], [9,1,4,4]));
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
