fa1a75ba3b4d09b5578463d950041085751171c2
Greg Stephen <GregAStephen@gmail.com>
2020-03-01 14:47:06 -0600

    added multiple pointers example

--- src/App/App.js
+++ src/App/App.js
@@ -4,7 +4,11 @@ import './App.css';
 import ProblemSolvingPatterns from './ProblemSolvingPatterns';
 
 function App() {
- console.error(ProblemSolvingPatterns.same([1,2,3,2], [9,1,4,4]));
+ console.error('same([1,2,3,2], [9,1,4,4])', ProblemSolvingPatterns.same([1,2,3,2], [9,1,4,4]));
+ console.error('same([1,2,2,2], [9,1,1,1])', ProblemSolvingPatterns.same([1,2,2,2], [9,1,1,1]));
+ console.error('validAnagram(hello, lloeh)', ProblemSolvingPatterns.validAnagram('hello', 'lloeh'));
+ console.error('validAnagram(bloop lloeh)', ProblemSolvingPatterns.validAnagram('bloop', 'lloeh'));
+ console.error('countUniqueValues([1,2,2,5,7,7,99])', ProblemSolvingPatterns.countUniqueValues([1,2,2,5,7,7,99]));
   return (
     <div className="App">
     </div>
--- src/App/ProblemSolvingPatterns.js
+++ src/App/ProblemSolvingPatterns.js
@@ -1,4 +1,7 @@
+/////////////// FREQUENCY COUNTER PATTERN //////////////////
+
 function same(arr1, arr2) {
+  // If not the same length then obviously not the same
   if (arr1.length !== arr2.length){
     return false;
   }
@@ -18,5 +21,52 @@ function same(arr1, arr2) {
   return true
 }
 
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
 
-export default {same};
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+
+/////////////////////////////////////////////////
+//////////////// MULTIPLE POINTERS //////////////
+/////////////////////////////////////////////////
+
+// takes in an array of SORTED numbers and figures out how many of the values are unique
+
+function countUniqueValues(arr){
+  if(arr.length === 0) return 0;
+  var i = 0;
+  for(var j = 1; j < arr.length; j++){
+    // loops thru the array ONCE checking to see if the value of wherever i is the same as the wherever j is
+      if(arr[i] !== arr[j]){
+        // if it is NOT the same then i increases by one and the new value of where it is located in the array
+        // is what the new number was
+          i++;
+          arr[i] = arr[j]
+      }
+  }
+  // to figure out the total number of unique values, just return the location of i + 1
+  return i + 1;
+}
+
+export default {same, validAnagram, countUniqueValues};
