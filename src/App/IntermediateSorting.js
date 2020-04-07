bbc8fb0bde622b4d799f1fc637b30edab37e716d
Greg Stephen <GregAStephen@gmail.com>
2020-04-07 11:59:15 -0500

    RADIX SORT

--- src/App/IntermediateSorting.js
+++ src/App/IntermediateSorting.js
@@ -1,4 +1,4 @@
-// these improve time complexity from O(n sq) to O(n log n)
+// these improve time complexity from O(n^2) to O(n log (n))
 
 /////////////////////////////////////////
 /////////////// MERGE SORT //////////////
@@ -49,10 +49,10 @@ const mergeSort = (arr) => {
 
 
 /* 
-All time complexity is O(n log n)
+All time complexity is O(n log (n))
 As n grows we have O(n) for merge
 The decompose of MergeSort is O(log n)
-That makes it O(n log n)
+That makes it O(n log (n))
 Space complexity is O(n)
 As we have a larger array we have to use more space
  */
@@ -84,10 +84,56 @@ const pivot = (array, start = 0, end = array.length + 1) => {
 }
 
 const quickSort = (array, left = 0, right = array.length - 1) => {
-  if(left < right) {
+  if (left < right) {
     let pivotIndex = pivot(arr, left, right)
     quickSort(array, left, pivotIndex - 1)
     quickSort(array, pivotIndex + 1, right)
   }
   return array;
+}
+
+
+
+////////////////////////////////////
+/////////// RADIX SORT /////////////
+///////////////////////////////////
+
+/* 
+Sorting algorithm that never makes comparison
+Works on lists of numbers
+Exploits the fact that info about the size of a number
+is encoded in the number of digits
+
+TIME COMPLEXITY = O(nk) - n = length of numbers, k = number of digits 
+SPACE COMPLEXITY = O(n + k)
+*/
+
+// Helper funtions
+const getDigit = (num, place) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+const digitCount = (num) => {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+const mostDigits = (nums) => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits;
+}
+
+const radixSort = (arr) => {
+  let largestDigit = mostDigits(arr);
+  for (let k = 0; k < largestDigit; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < arr.length; i++) {
+      digitBuckets[getDigit(arr[i], k)].push(arr[i]);
+    }
+    arr = [].concat(...digitBuckets);
+  }
+  return arr;
 }
