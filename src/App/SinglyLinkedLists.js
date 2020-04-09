19303306d24b28d0e92628a1b7638de392001715
Greg Stephen <GregAStephen@gmail.com>
2020-04-09 12:28:44 -0500

    Singly Linked Lists

--- src/App/SinglyLinkedLists.js
+++ src/App/SinglyLinkedLists.js
@@ -48,4 +48,133 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    newTail.next = null;
+    this.tail = newTail;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+      this.head = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) {
+      return null;
+    }
+    let counter = 0;
+    let itemToReturn = this.head;
+    while (counter !== index) {
+      itemToReturn = itemToReturn.next;
+      counter++;
+    }
+    return itemToReturn;
+  }
+
+  set(index, value) {
+    let valueToChange = this.get(index);
+    if (!valueToChange) {
+      return false;
+    }
+    valueToChange = value;
+    return true;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    if (index === this.length) {
+      this.push(value);
+      return true;
+    }
+    if (index === 0) {
+      this.unshift(value);
+      return true;
+    }
+    let newNode = new Node(value);
+    let prevNode = this.get(index - 1)
+    let nextNode = prevNode.next;
+    prevNode.next = newNode;
+    newNode.next = nextNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) {
+      return undefined;
+    }
+    if (index === this.length - 1) {
+      return this.pop();
+    }
+    if (index === 0) {
+      return this.shift();
+    }
+    let previousNode = this.get(index - 1);
+    let nodeToDelete = previousNode.next;
+    previousNode.next = nodeToDelete.next;
+    this.length--;
+    return nodeToDelete;
+  }
+
+  reverse() {
+    let next;
+    let prev = null;
+    let current = this.head;
+    this.head = this.tail;
+    this.tail = current;
+    for(let i = 0; i < this.length; i++) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    return this;
+  }
 }
+
+
+/*
+BIG O
+ Insertion = O(1)
+ Removal = it depends best case is O(1) if its at beginning or O(n)
+ Searching = O(n)
+ Access = O(n)
+*/
