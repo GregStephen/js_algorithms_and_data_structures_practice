6e9aa8a10d531b1518907cfe2d360bcc7d0ea3ea
Greg Stephen <GregAStephen@gmail.com>
2020-04-23 11:53:09 -0500

    Added basic tree info

---
+++ src/App/Trees.js
@@ -0,0 +1,24 @@
+/*
+- A Data structure that consists of nodes in a parent/child relationship
+- Nonlinear
+- Must have a single ROOT
+
+- Root - the top node in a tree
+- Child - A node directly connected to another node when moving
+  away from the Root
+- Parent - the converse nodtion of a Child
+- Siblings - A group of nodes with the same Parent
+- Leaf - A node with no children
+- Edge - The connection between one node and another
+
+*/
+
+/*
+EXAMPLES
+- HTML DOM
+- Network routing
+- Abstract Syntax trees
+- Artificial Intelligence
+- Computer File Systems
+- JSON
+*/
