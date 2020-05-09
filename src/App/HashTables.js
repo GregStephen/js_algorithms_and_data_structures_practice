511bde8b0b461672f96f1885aafa86f82cab7c50
Greg Stephen <GregAStephen@gmail.com>
2020-05-09 15:08:01 -0500

    intro to hash tables

---
+++ src/App/HashTables.js
@@ -0,0 +1,13 @@
+/*
+What is a Hash Table
+
+Hash tables are used to store key-value pairs
+
+they are like arrays ,but the keys are not ordered
+
+Unlike arrays, hash tables are FAST for all of the following:
+  finding values, adding new values and removing values
+
+Objects and Maps in JS
+Dictionaries in Python
+*/
