2f197aa61eb05beb822b4c9be5a998197c008b24
Greg Stephen <GregAStephen@gmail.com>
2020-05-09 15:16:09 -0500

    intro to hash function

--- src/App/HashTables.js
+++ src/App/HashTables.js
@@ -10,4 +10,20 @@ Unlike arrays, hash tables are FAST for all of the following:
 
 Objects and Maps in JS
 Dictionaries in Python
+
+To implement a hash table we'll use an array
+
+In order to look up values by key, need a way to 
+CONVERT KEYS INTO VALID ARRAY INDICES
+A function that performs this task is called a 
+HASH FUNCTION
+
+*/
+
+/*
+HASH FUNCTION
+What makes a good hash?
+- FAST (i.e. constant time)
+- Doesn't cluster outputs at specific indices, but distributes uniformly
+- Deterministic (same input yields same output)
 */
