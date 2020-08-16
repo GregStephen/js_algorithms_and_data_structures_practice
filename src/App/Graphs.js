f092c31a0a71d81e0bb30ea0fcd9d187c742af93
Greg Stephen <GregAStephen@gmail.com>
2020-08-16 15:58:39 -0500

    graph types and examples

---
+++ src/App/Graphs.js
@@ -0,0 +1,45 @@
+/*
+GRAPHS 
+A collection of nodes and the connections between them
+Freeform, no pattern
+
+A tree is a type of a graph
+*/
+
+
+/*
+Uses for graphs
+- Social Networks
+- Location Mapping
+- Routing Algorithms
+- Recommendations - 'People also watched', 'Frequently bought with'
+*/
+
+/*
+Terms
+- Vertex - a node
+- Edge - connection between nodes
+- Weighted/Unweighted - a value assigned to an edge/ or not
+- Directed/Undirected - a direction assigned to an edge/ or not
+*/
+
+
+/*
+Types of Graphs
+
+Undirected Graph
+- No direction associated with an edge
+  - i.e. facebook friends, you see both pages
+
+Directed Graph
+- One way with the edge, a direction is assigned
+  - i.e. Instagram followers, you can follow but not be followed
+
+Weighted
+- The edges have a value
+
+Unweighted
+- The edges have no value
+
+
+*/
